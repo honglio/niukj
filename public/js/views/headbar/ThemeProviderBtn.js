99a09fad69e616c73f719ac17a1cb75cd0681bbc
honglio <hong.liu@live.cn>
2014-06-12 20:04:18 +0800

    editor and index ready.

---
+++ public/js/views/headbar/ThemeProviderBtn.js
@@ -0,0 +1,20 @@
+define(["CustomView"],
+function(CustomView) {
+	"use strict";
+    return CustomView.extend({
+        className: 'btn btn-primary dropdown-toggle',
+        tagName: 'a',
+        id: 'themeProvider',
+        events: {
+            destroyed: 'remove'
+        },
+        attributes: {'data-toggle': "dropdown",
+                     'data-target': "#dropdown-menu"
+                    },
+
+        render: function() {
+            this.$el.html('<i class="icon-gear"></i><strong>背景</strong>');
+            return this;
+        }
+    });
+});
