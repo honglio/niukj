bf7ce2e20b9a9e11a42219b90e08a5e0d5ec013c
honglio <hong.liu@live.cn>
2014-07-21 15:57:39 +0800

    Remove the alert when save successfully. Add alert bar to display the 'save successfully' message.

--- public/js/views/storage/StorageModal.js
+++ public/js/views/storage/StorageModal.js
@@ -15,6 +15,7 @@ define(["CustomView",
 
         initialize: function() {
             this.storageInterface = new StorageInterface();
+            this.hideAlert();
         },
 
         render: function() {
@@ -42,13 +43,27 @@ define(["CustomView",
             }
         },
 
+        showAlert: function(title, text, klass) {
+            $('.alert').removeClass("alert-error alert-warning alert-success alert-info");
+            $('.alert').addClass(klass);
+            $('.alert').html('<button class="close" data-dismiss="alert">×</button><strong>' + title + '</strong> ' + text);
+            $('.alert').show('fast');
+            setTimeout(this.hideAlert, 4000 );
+        },
+
+        hideAlert: function() {
+            $('.alert').hide();
+        },
+
         _okClicked: function(e) {
-			console.log('_okClicked');
             if(e.preventDefault){
                 e.preventDefault();
             } else {
                 e.returnValue = false;
             }
+
+            this.$('.ok').text('保存中...');
+
             var self = this;
             var filename = this.$input.val();
 
@@ -63,16 +78,16 @@ define(["CustomView",
                 this.storageInterface.store(serialized, function(id) {
                     if(id) {
                         self.$el.modal('hide');
-                        alert('保存成功');
-                        window.location.replace("/articles/" + id + '/edit');
+                        self.showAlert('成功！', '课件已保存成功', 'alert-success');
+                        // window.location.replace("/articles/" + id + '/edit');
                     }
                 });
             } else {
                 this.storageInterface.saveAs(serialized, function(id) {
                     if(id) {
                         self.$el.modal('hide');
-                        alert('保存成功');
-                        window.location.replace("/articles/" + id + '/edit');
+                        self.showAlert('成功！', '课件已保存成功', 'alert-success');
+                        // window.location.replace("/articles/" + id + '/edit');
                     }
                 });
             }
