3b8dbc66ada507717233885d4d03e8fef9174c26
honglio <hong.liu@live.cn>
2015-09-17 11:30:58 +0800

    Sometimes save deck failed.
    Close #125

--- public/js/views/slide_components/drawers/ImageDrawer.js
+++ public/js/views/slide_components/drawers/ImageDrawer.js
@@ -8,15 +8,17 @@ define(["underscore", "./AbstractDrawer"], function(_, AbstractDrawer) {
     _.extend(ImageModelDrawer.prototype, AbstractDrawer.prototype);
 
     ImageModelDrawer.prototype.paint = function(imageModel) {
-        this._imageLoaded(imageModel._cachedImage, imageModel);
+        if (imageModel._cachedImage) {
+            this._imageLoaded(imageModel._cachedImage, imageModel);
+        }
     };
 
     ImageModelDrawer.prototype._imageLoaded = function(image, imageModel) {
         var bbox = {
-            x: imageModel.get('x') * this.scale.x,
-            y: imageModel.get('y') * this.scale.y,
-            width: image.naturalWidth * this.scale.x * 1.5,
-            height: image.naturalHeight * this.scale.y * 1.5
+            x: (imageModel.x ? imageModel.x : imageModel.get('x')) * this.scale.x,
+            y: (imageModel.y ? imageModel.y : imageModel.get('y')) * this.scale.y,
+            width: (image.naturalWidth ? image.naturalWidth : 0) * this.scale.x * 1.5,
+            height: (image.naturalHeight ? image.naturalHeight: 0) * this.scale.y * 1.5
         };
         this.applyTransforms(imageModel, bbox);
         this.g2d.drawImage(image, bbox.x, bbox.y, bbox.width, bbox.height);
