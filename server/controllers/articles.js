b9866bab9636a300cdb621e98899c18da68eb292
honglio <hong.liu@live.cn>
2015-09-21 21:04:54 +0800

    Style cleanup in Profile page

--- server/controllers/articles.js
+++ server/controllers/articles.js
@@ -295,15 +295,15 @@ exports.present = function(req, res) {
     });
 
     // if (req.user) {
-        // if (lastUser[req.user.email] == null) {
-        //     lastUser[req.user.email] = [];
-        // }
+    // if (lastUser[req.user.email] == null) {
+    //     lastUser[req.user.email] = [];
+    // }
 
-        // if (lastUser[req.user.email].indexOf(req.article.fileName) === -1) {
-            req.article.hitCounter += 1;
-            req.article.save();
-            // lastUser[req.user.email].push(req.article.fileName);
-        // }
+    // if (lastUser[req.user.email].indexOf(req.article.fileName) === -1) {
+    req.article.hitCounter += 1;
+    req.article.save();
+    // lastUser[req.user.email].push(req.article.fileName);
+    // }
     // }
 
     res.render('article/present', {
